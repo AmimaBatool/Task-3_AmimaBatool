6d496b857847da29b6f0a9b6af88ef55a5d30912
Amima Batool <amimabatool20@gmail.com>
2026-09-06 17:06:09 +0500

    Add files via upload

---
+++ Random_Password_Generator.py
@@ -0,0 +1,26 @@
+#=====Random Password Generator=====
+
+import random
+import string
+
+print("------Random Password Generator------")
+print()
+
+print("Words used to generate password: ")
+print(string.ascii_letters)
+print(string.digits)
+print(string.punctuation)
+
+print()
+length = int(input("Enter Length: "))
+print()
+
+chars = string.ascii_letters + string.digits + string.punctuation
+
+password = ""
+
+for i in range(length):
+    password += random.choice(chars)
+
+print("Random Password: ", password)
+print()
